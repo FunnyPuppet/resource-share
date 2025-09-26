f2383dadb63a3d68762f2dca4a3fbbe29588f0bd
liud <ld.puppet.843@gmail.com>
2025-09-26 17:41:07 +0800

    2025-09-26 update

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -14,7 +14,7 @@ export function generateStaticParams() {
 
 type LayoutProps = {
   children: React.ReactNode,
-  params: { locale: string }
+  params: Promise<any>
 }
 
 export async function generateMetadata(
